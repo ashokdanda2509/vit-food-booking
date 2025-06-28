de6eb08a1e8b56761a820996833a664ad3eff2ee
Danda, Ashok - Contractor <adanda+EADev@contractor.ea.com>
2025-06-28 14:52:37 +0530

    first booking

---
+++ FoodBooking.java
@@ -0,0 +1,8 @@
+public class FoodBooking {
+    public static void main(String[] args) {
+        System.out.println("I am booking my order");      // Call the bookFood method to simulate booking foo
+    }
+    public static void bookFood() {
+        System.out.println("Food booked successfully!");  // Print confirmation message
+    }
+}
